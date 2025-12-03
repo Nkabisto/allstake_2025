122d0906781741c021deaa3c204f6ea760cb079c
Lionel <nkabisto@gmail.com>
2025-12-03 13:19:48 +0200

    Ingested four staging_tables from Postgres

--- allstake_2025.py
+++ allstake_2025.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import polars as pl
 from dotenv import load_dotenv
 import psycopg
+from psycopg import connection
 import logging
 
 # Log events of interest
@@ -16,6 +17,17 @@ logger.addHandler(console_handler)
 
 load_dotenv()
 
+def ingest_tables(table_name:str, con:connection)->pl.DataFrame:
+    try:
+        df = pl.read_database(query=f'SELECT * FROM {table_name}',connection=con)
+    except Exception as e:
+        raise RuntimeError("Database read failed") from e
+
+    print(df.sample(min(10,df.height)))
+    print(df.columns)
+
+    return df
+
 if __name__ == "__main__":
     db_name = os.getenv("DB_NAME")
     db_host= os.getenv("DB_HOST")
@@ -26,8 +38,10 @@ if __name__ == "__main__":
     conn_string = f"dbname={db_name} user={db_user} password={db_pwd} host={db_host} port={db_port}"
     try:
         with psycopg.connect(conn_string) as con:
-            df = pl.read_database(query='SELECT * FROM staging_booking_tb', connection=con)
-            print(df)
+            tables = ['staging_booking_tb', 'staging_financials_tb', 'staging_jobs_tb','staging_stocktaker_tb']
+            for tb in tables:
+                df = ingest_tables(tb,con)
+
     except Exception as e:
         logger.error(f"Database connection failed: {e}")
         logger.error("Make sure PostgreSQL is running: sudo systemctl start postgresql")
